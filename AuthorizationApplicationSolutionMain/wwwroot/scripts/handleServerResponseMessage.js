c19f78db9264ceecd0ae1ceb0547f6566eb772a3
Kirill <sosisks888@mail.ru>
2023-10-27 13:00:33 +0300

    add succsess and error message on authorization page

---
+++ AuthorizationApplicationSolutionMain/wwwroot/scripts/handleServerResponseMessage.js
@@ -0,0 +1,59 @@
+function catchResponseMessage(response) {
+    const cutFirstPartMassege = response.split('{');
+    const cutSecondPartMessage = cutFirstPartMassege[1].split('}');
+    const userInformation = cutSecondPartMessage[0]
+    const deleteSimbols = /\*|"|,|\$/g;
+    const newString = userInformation.replace(deleteSimbols, '');
+    const split = newString.split(':');
+
+    const firstNameSplit = split[2].split('L');
+    const firstName = firstNameSplit[0]; 
+    console.log(`FIRSTNAME: ${firstName}`);
+
+    const secondNameSplit = split[3].split('C');
+    const secondName = secondNameSplit[0];
+    console.log(`SECONDNAME: ${secondName}`);
+
+    const mobileSplit = split[10].split('E');
+    const mobile = mobileSplit[0];
+    console.log(`MOBILE: ${mobile}`);
+
+    const emailSplit = split[11].split('E');
+    const email = emailSplit[0];
+    console.log(`EMAIL: ${email}`);
+
+    const countryIdSplit = split[14].split('S');
+    const countryId = countryIdSplit[0];
+    console.log(`COUNTRYID: ${countryId}`);
+    
+    const result = [firstName,secondName,mobile,email,countryId];
+    return result;
+}
+
+function foundUser(response) {
+    const cutFirstPartMassege = response.split('{');
+    const cutSecondPartMessage = cutFirstPartMassege[1].split('}');
+    const splitResultCode = cutSecondPartMessage[0].split(':');
+    const resultCode = splitResultCode[1].split(',');
+    let myAlertError;
+    let myAlertSuccess;
+    if(resultCode[0] == "-1") {
+        myAlertSuccess  = document.getElementById('myAlertSuccess');
+        myAlertSuccess.style.display = "none";
+        myAlertError = document.getElementById('myAlertError');
+        myAlertError.style.display = "block";
+    }
+    else { 
+        myAlertError = document.getElementById('myAlertError');
+        myAlertError.style.display = "none";
+        myAlertSuccess  = document.getElementById('myAlertSuccess');
+        myAlertSuccess.style.display = "block";
+        let userInfo = catchResponseMessage(response);
+
+        let t = document.createElement("a");
+        t.append("FirstName: " + userInfo[0] + ",");
+        t.append("LastName: " + userInfo[1] + ",");
+        document.getElementById('successResponse').append(t);
+    }
+}
+
--- AuthorizationApplicationSolutionMain/wwwroot/scripts/request.js
+++ AuthorizationApplicationSolutionMain/wwwroot/scripts/request.js
@@ -1,4 +1,6 @@
 async function sendRequest() {
+    const username = document.getElementById('usernameInput').value;
+    const password = document.getElementById('passwordInput').value;
     const xhr = new XMLHttpRequest();
     const method = 'POST';
     const url = 'http://isapi.icu-tech.com/icutech-test.dll/soap/IICUTech';
@@ -6,39 +8,61 @@ async function sendRequest() {
     xhr.open(method, url, true);
 
     xhr.onload = () => {
-        let response = xhr.response;
-        let responseMessage = catchServerResponseMessage(response);
-        if(responseMessage == "\"User not found\"")
-        {
-            console.log('nice work');
-        }
-        
+        const response = xhr.response;
+        const responseMessage = foundUser(response);
+        console.log(responseMessage);
     }
-    xhr.send(getBodyXmlRequest());
+
+    xhr.send(getBodyXmlRequest(username,password));
 }
 
-function catchServerResponseMessage(response) {
-    let cutFirstPartMassege = response.split('{');
-    let cutSecondPartMessage = cutFirstPartMassege[1].split('}');
-    let userInformation = cutSecondPartMessage[0].split(':');
-    return userInformation[2];
-}
+function getBodyXmlRequest(username,password) {
+    const xml = '' +
+    '<?xml version="1.0" encoding="UTF-8"?>' +
+    '<env:Envelope xmlns:env="http://www.w3.org/2003/05/soap-envelope" '+
+    '	xmlns:ns1="urn:ICUTech.Intf-IICUTech" ' +
+    '	xmlns:xsd="http://www.w3.org/2001/XMLSchema" ' +
+    '	xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" ' +
+    '	xmlns:enc="http://www.w3.org/2003/05/soap-encoding">' +
+    '	<env:Body>' +
+    '		<ns1:Login env:encodingStyle="http://www.w3.org/2003/05/soap-encoding">' +
+    `			<UserName xsi:type="xsd:string">${username}</UserName>` +
+    `			<Password xsi:type="xsd:string">${password}</Password>` +
+    '			<IPs xsi:type="xsd:string"></IPs>' +
+    '		</ns1:Login>' +
+    '	</env:Body>' +
+    '</env:Envelope>';
 
-function getBodyXmlRequest(){
-    let xml = '' +
-'<?xml version="1.0" encoding="UTF-8"?>' +
-'<env:Envelope xmlns:env="http://www.w3.org/2003/05/soap-envelope" '+
-'	xmlns:ns1="urn:ICUTech.Intf-IICUTech" ' +
-'	xmlns:xsd="http://www.w3.org/2001/XMLSchema" ' +
-'	xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" ' +
-'	xmlns:enc="http://www.w3.org/2003/05/soap-encoding">' +
-'	<env:Body>' +
-'		<ns1:Login env:encodingStyle="http://www.w3.org/2003/05/soap-encoding">' +
-'			<UserName xsi:type="xsd:string">Wwww</UserName>' +
-'			<Password xsi:type="xsd:string">Gxshebsv</Password>' +
-'			<IPs xsi:type="xsd:string"></IPs>' +
-'		</ns1:Login>' +
-'	</env:Body>' +
-'</env:Envelope>';
     return xml;
 }
+
+function createNewUser(username,password) {
+    const xml = '' +
+    '<?xml version="1.0" encoding="UTF-8"?>' +
+    '<env:Envelope xmlns:env="http://www.w3.org/2003/05/soap-envelope" ' +
+    '   xmlns:ns1="urn:ICUTech.Intf-IICUTech" ' +
+    '   xmlns:xsd="http://www.w3.org/2001/XMLSchema" ' +
+    '   xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" ' +
+    '   xmlns:enc="http://www.w3.org/2003/05/soap-encoding">' +
+    '   <env:Body>' +//igor.gulyaev.92@list.ru
+    '       <ns1:RegisterNewCustomer env:encodingStyle="http://www.w3.org/2003/05/soap-encoding">' +
+    `           <Email xsi:type="xsd:string">${username}</Email>` +
+    '           <FirstName xsi:type="xsd:string">DavidS</FirstName>' +
+    '           <LastName xsi:type="xsd:string">Shmactrertaswsw</LastName>' +
+    `			<Password xsi:type="xsd:string">${password}</Password>` + 
+    '           <Mobile xsi:type="xsd:string">89182999732</Mobile>' +
+    '           <CountryID xsi:type="xsd:int">1</CountryID>' +
+    '           <aID xsi:type="xsd:int">2</aID>' +
+    '           <SignupIP xsi:type="xsd:string">167.34.99.72</SignupIP>' +
+    '       </ns1:RegisterNewCustomer>' +
+    '   </env:Body>' +
+    '</env:Envelope>';
+
+    const xhr = new XMLHttpRequest();
+    const method = 'POST';
+    const url = 'http://isapi.icu-tech.com/icutech-test.dll/soap/IICUTech';
+
+    xhr.open(method, url, true);
+
+    xhr.send(xml);
+};
